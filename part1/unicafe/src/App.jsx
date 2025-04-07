00bf8ce9cbd17b418dbfaf30c77a5ee2b6b3cc38
kokiprogramador <squirrelandrendy@gmail.com>
2025-04-07 08:10:08 -0400

    1.7 and 1.8 exercises of UniCafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,10 +1,42 @@
 import { useState } from "react";
 
+const Display = ({ good, neutral, bad, total, average, positive }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td scope="row">
+            Good: <br /> {good}
+          </td>
+          <td scope="row">
+            Neutral: <br /> {neutral}
+          </td>
+          <td scope="row">
+            Bad: <br /> {bad}
+          </td>
+          <td scope="row">
+            All: <br /> {total}
+          </td>
+          <td scope="row">
+            Average: <br /> {average}
+          </td>
+          <td>
+            Positive <br />
+            {positive} %
+          </td>
+        </tr>
+      </thead>
+    </table>
+  );
+};
+
 function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
+  const total = good + neutral + bad;
+  const average = total / 3;
+  const positive = (good + neutral * 100) / 100;
   return (
     <div>
       <button onClick={() => setGood(good + 1)}>good</button>
@@ -12,15 +44,14 @@ function App() {
       <button onClick={() => setBad(bad + 1)}>bad</button>
       <br />
       <br />
-      <table>
-        <thead>
-          <tr>
-            <th>Good {good}</th>
-            <th>Neutral {neutral}</th>
-            <th>Bad {bad}</th>
-          </tr>
-        </thead>
-      </table>
+      <Display
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+        positive={positive}
+      />
     </div>
   );
 }
